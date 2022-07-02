dfe2fa8258b231f85345c47186da6df19155e13a
AlinaShestak <alinashestak08@gmail.com>
2022-07-03 01:03:16 +0300

    0

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import { ButtonExample, List, CustomTitle, ButtonMenuBurger, Link, SearchButton, AboutUserNotAuth, AboutUserAuth, LikeButton, DislikeButton } from './components/examples';
+import { ButtonExample, List} from './components/examples';
 
 function App() {
   const onClick = () => {
@@ -17,20 +17,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <ButtonExample text="Button example" onClick={onClick} />
-        <List list={users} /> */}
-        <CustomTitle text="Sign in"></CustomTitle>
-        <ButtonMenuBurger onClick={onClick} active={false}></ButtonMenuBurger>
-        <Link url='https://99px.ru/sstorage/53/2014/03/tmb_98216_2248.jpg' onClick={onClick} text="Ссылка"></Link>
-        <SearchButton onClick={onClick}></SearchButton>
-        <p>--</p>
-        <AboutUserNotAuth onClick={onClick}></AboutUserNotAuth>
-        <p>--</p>
-        <AboutUserAuth initials="SA" username="Shestak Alina" onClick={onClick}></AboutUserAuth>
-        <p>--</p>
-        <LikeButton onClick={onClick}></LikeButton>
-        <p>--</p>
-        <DislikeButton onClick={onClick}></DislikeButton>
+        <ButtonExample text="Button example" onClick={onClick} />
+        <List list={users} />
       </header>
     </div>
   );
