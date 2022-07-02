0ed08b657789dcd236240a2a85de5e2e52f5811a
AlinaShestak <alinashestak08@gmail.com>
2022-07-03 01:19:48 +0300

    04

--- src/components/examples/index.ts
+++ src/components/examples/index.ts
@@ -2,10 +2,3 @@
 export { List } from './List/List';
 export { ButtonExample } from './ButtonExample/ButtonExample';
 export { CustomTitle } from './CustomTitle/CustomTitle';
-export { ButtonMenuBurger } from './ButtonMenuBurger/ButtonMenuBurger'; 
-export { Link } from './Link/Link';
-export { SearchButton } from './SearchButton/SearchButton';
-export { AboutUserNotAuth } from './AboutUserNotAuth/AboutUserNotAuth';
-export { AboutUserAuth } from './AboutUserAuth/AboutUserAuth';
-export { LikeButton } from './LikeButton/LikeButton';
-export { DislikeButton } from './DislikeButton/DislikeButton';
